9d6b565967337843fbcc5cb922e6cdbccfb57e70
Martin Nusspaumer <142355408+KlassicStudio@users.noreply.github.com>
2023-10-28 01:34:44 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [task, setTask] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-3 text-center">
+			<h1 className="mt-4">ToDo</h1>
+			<h5 className="my-3">Task organizer</h5>
+			<ul class="list-group d-flex align-items-center">
+			<li class="list-group-item d-flex align-items-center py-3">
+			     <input
+				  type="text"
+				  class="form-control me-5"
+				  onChange={(e) => setInputValue(e.target.value)}
+				  placeholder="Type your task"
+				  value={inputValue}
+				  onKeyUp={(e) => {
+					if(e.key === "Enter") {
+						setTask(task.concat(inputValue));
+						setInputValue("");
+					}
+				  }}
+				  aria-label="Recipient's username"/>
+                 <button type="button" class="btn btn-primary float-end ms-auto btn-sm btn-pad">Add</button>
+				
+				</li>
+				
+				{task.map((item, index) => (
+        
+						 <li className="list-group-item d-flex align-items-center py-3">
+						 <input className="form-check-input me-3" type="checkbox" value="" aria-label="" />
+						 {item}
+						 <button
+							type="button"
+							onClick={() => {
+								setTask(
+									task.filter(
+										(t, currentIndex) =>
+										index != currentIndex
+									)
+								)
+							}}
+							className="btn btn-danger float-end ms-auto btn-sm">
+							Delete
+						 </button>
+					 </li> 
+                 ))}
+
+				<div className="list-group-item d-flex align-items-center py-3 t-counter">4 tasks</div>
+				</ul>
+				
 		</div>
 	);
 };
